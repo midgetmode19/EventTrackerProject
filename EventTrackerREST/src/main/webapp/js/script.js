4e3add79016dfb43b64303d7977b57e54847ac03
Samantha Janik <samajanik@gmail.com>
2018-09-23 18:56:23 -0600

    delete functional but does not immediately update on the page

--- EventTrackerREST/src/main/webapp/js/script.js
+++ EventTrackerREST/src/main/webapp/js/script.js
@@ -6,10 +6,11 @@ window.addEventListener('load', function(e) {
 function init() {
   document.viewEntries.viewLog.addEventListener('click', viewEntries);
   document.entryForm.submit.addEventListener('click', addEntry);
+  document.addEventListener('click', updateEntry);
+  document.addEventListener('click', deleteEntry);
 }
 
 function viewEntries(e) {
-  e.preventDefault();
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "/api/entries");
   xhr.onreadystatechange = function() {
@@ -34,6 +35,7 @@ function displayEntries(entries) {
 
   var trh = document.createElement('tr');
   var thead = document.createElement('thead');
+  var thId = document.createElement('th');
   var thMiles = document.createElement('th');
   var thCost = document.createElement('th');
   var thGallons = document.createElement('th');
@@ -41,11 +43,13 @@ function displayEntries(entries) {
 
   var tbody = document.createElement('tbody');
 
+  thId.textContent = "Entry Number";
   thMiles.textContent = "Miles";
   thCost.textContent = "Fuel Cost";
   thGallons.textContent = "Gallons This Fill";
   thDate.textContent = "Date of Refuel";
 
+  trh.appendChild(thId);
   trh.appendChild(thMiles);
   trh.appendChild(thCost);
   trh.appendChild(thGallons);
@@ -56,84 +60,115 @@ function displayEntries(entries) {
 
   entries.forEach(function(value, i, arr) {
     var tr = document.createElement('tr');
+    var tdId = document.createElement('td');
     var tdMiles = document.createElement('td');
     var tdCost = document.createElement('td');
     var tdGallons = document.createElement('td');
     var tdDate = document.createElement('td');
+    var tdUpdate = document.createElement('td');
+    var tdDelete = document.createElement('td');
+    var tdUpdateButton = document.createElement('input');
+    tdUpdateButton.type = 'submit';
+    tdUpdateButton.value = 'Edit Entry';
+    tdUpdateButton.id = 'updatebtn';
+    var tdDeleteButton = document.createElement('input');
+    tdDeleteButton.type = 'button';
+    tdDeleteButton.value = 'Delete';
+    tdDeleteButton.id = 'deletebtn';
 
+
+    tdId.textContent = value.id;
     tdMiles.textContent = value.miles;
     tdCost.textContent = "$" + value.fuelCost;
     tdGallons.textContent = value.gallonsPerFill;
     tdDate.textContent = value.refuelDate;
 
+    tr.appendChild(tdId);
     tr.appendChild(tdMiles);
     tr.appendChild(tdCost);
     tr.appendChild(tdGallons);
     tr.appendChild(tdDate);
+    tr.appendChild(tdUpdateButton);
+    tr.appendChild(tdDeleteButton);
     tbody.appendChild(tr);
 
   });
   table.appendChild(tbody);
   div.appendChild(table);
+
   document.viewEntries.viewLog.removeEventListener('click', viewEntries);
 }
 
 function addEntry(e) {
   e.preventDefault();
-	var form = e.target.parentElement;
-	var miles = form.miles.value;
-	var cost = form.fuelCost.value;
-	var gallons = form.gallonsPerFill.value;
-	var refuelDate = form.refuelDate.value;
+  var form = e.target.parentElement;
+  var miles = form.miles.value;
+  var cost = form.fuelCost.value;
+  var gallons = form.gallonsPerFill.value;
+  var refuelDate = form.refuelDate.value;
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'api/entries', true);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'api/entries', true);
 
-	xhr.setRequestHeader("Content-type", "application/json");
+  xhr.setRequestHeader("Content-type", "application/json");
 
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState === 4 ) {
-	    if ( xhr.status == 200 || xhr.status == 201 ) {
-	      var data = JSON.parse(xhr.responseText);
-	      console.log(data);
-	    }
-	    else {
-	      console.log("POST request failed.");
-	      console.error(xhr.status + ': ' + xhr.responseText);
-	    }
-	  }
-	};
+  var data;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status == 200 || xhr.status == 201) {
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+      } else {
+        console.log("POST request failed.");
+        console.error(xhr.status + ': ' + xhr.responseText);
+      }
+    }
+  };
+  var entry = {
+    "miles": form.miles.value,
+    "fuelCost": form.fuelCost.value,
+    "gallonsPerFill": form.gallonsPerFill.value,
+    "refuelDate": form.refuelDate.value
+  }
+  var entryJson = JSON.stringify(entry);
+  xhr.send(entryJson);
 
+  form.reset();
+}
 
-	var div = document.getElementById("allEntries");
-	var table = document.getElementById('entriesTable');
-	var tbody = document.createElement('tbody');
-	var tr = document.createElement('tr');
-	var tdMiles = document.createElement('td');
-	var tdCost = document.createElement('td');
-	var tdGallons = document.createElement('td');
-	var tdDate = document.createElement('td');
+function updateEntry(e) {
+  if (e.target && e.target.id == 'updatebtn') {
+    console.log("update click");
 
-	tdMiles.textContent = miles;
-	tdCost.textContent = "$" + cost;
-	tdGallons.textContent = gallons;
-	tdDate.textContent = refuelDate;
+  }
+}
 
-	tr.appendChild(tdMiles);
-	tr.appendChild(tdCost);
-	tr.appendChild(tdGallons);
-	tr.appendChild(tdDate);
-	tbody.appendChild(tr);
-	table.appendChild(tbody);
-	div.appendChild(table);
-	// form.reset();
+function deleteEntry(e) {
+  if (e.target && e.target.id == 'deletebtn') {
+    console.log("delete click");
+		var entryId = e.target.parentElement.firstChild.textContent;
+		console.log(entryId);
+    var xhr = new XMLHttpRequest();
+    xhr.open('DELETE', 'api/entries/' + entryId, true);
+		if (!isNaN(entryId) && entryId > 0) {
 
-	var entry = {
-		"miles" : form.miles.value,
-		"fuelCost" : form.fuelCost.value,
-		"gallonsPerFill" : form.gallonsPerFill.value,
-		"refuelDate" : form.refuelDate.value
-	}
-	var entryJson = JSON.stringify(entry);
-	xhr.send(entryJson);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status == 200 || xhr.status == 202) {
+          var data = JSON.parse(xhr.responseText);
+          console.log(data);
+        } else {
+          console.log("Delete request failed.");
+          console.error(xhr.status + ': ' + xhr.responseText);
+        }
+      }
+    };
+
+    xhr.send();
+  }
+}
+}
+
+function updateDeleteForm() {
+  document.createElement('form');
 }
